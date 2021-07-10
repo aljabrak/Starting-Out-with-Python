fbdd9ae4343daee12789c4f8f2d93ad64275e63d
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-10 13:20:04 +0500

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,59 @@
+# Starting Out with Python (4th Edition).
+# Tony Gaddis.
+# Page 306.
+# Q. 21 Rock, Paper, Scissors Game.
+
+from random import *
+
+MIN = 1
+MAX = 3
+
+print("----- ROCK, PAPER, SCISSORS GAME. -----")
+print("PRESS \n 1) Rock. \n 2) Paper. \n 3) Scissors.")
+
+m = 1
+
+while m == 1:
+    x = randint(1,3)
+    u = int(input("user = "))
+
+    if x == 1 and u == 3:
+        print("Rock Smashes Scissors.")
+        print("You Lost...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    elif u == 3 and x == 2:
+        print("Scissors Cuts Paper.")
+        print("You Won...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    elif u == 2 and x == 1:
+        print("Paper Wraps Rock.")
+        print("You Won...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    elif x == 3 and u == 1:
+        print("Rock Smashes Scissors.")
+        print("You Won...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    elif u == 2 and x == 3:
+        print("Scissors Cuts Paper.")
+        print("You Lost...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    elif u == 1 and x == 2:
+        print("Paper Wraps Rock.")
+        print("You Lost...")
+        print("Enter 1 to Start Again...")
+        m = int(input())
+
+    else:
+        print("Same Choice.")
+        print("Enter 1 to Start Again...")
+        m = int(input())
