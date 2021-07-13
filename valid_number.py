f2be7b7bee9a9ccdba1feebfb2c2ac8d662c1ddb
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-13 14:47:55 +0500

    Create valid_number.py

---
+++ valid_number.py
@@ -0,0 +1,23 @@
+# Starting Out with Python (4th Edition).
+# Tony Gaddis.
+# Page 424.
+# Q. 1 Valid Number Information.
+
+numbers = [74, 19, 105, 20, -2, 67, 77, 124, -45, 38]
+numbers.sort()
+print('Numbers = ', numbers)
+valid_numbers = []
+
+for x in numbers:
+    if x >= 0:
+        valid_numbers.append(x)
+valid_numbers.sort()
+print('Valid Numbers = ', valid_numbers)
+
+total = 0
+for x in valid_numbers:
+    total += x 
+    
+print('Total = ', total)
+average = total/len(valid_numbers)
+print('Average = ', average)
