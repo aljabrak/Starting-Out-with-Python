de905c60b30c75e0b2a5544014f6fb6cb20e20d6
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-25 01:07:51 +0500

    Update morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -5,38 +5,32 @@
 
 def morse_code(string):
     string = string.upper()
-    characters = [',', '.', '?', ';', ':', '/', '!', '+', '-', '$', '&', '@', '(', ')']
-    morse_code_characters = [
-        '--..--', '.-.-.-', '..--..', '-.-.-.',
-        '---...', '-..-.', '-.-.--', '.-.-.',
-        '-....-', '...-..-', '.-...', '.--.-.',
-        '-.--.', '-.--.-']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    morse_code_numbers = ['-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.']
-    alpahbets = [
+    character = [
+        ',', '.', '?', ';', ':', '/',
+        '!', '+', '-', '$', '&', '@',
+        '(', ')', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F',
         'G', 'H', 'I', 'J', 'K', 'L',
         'M', 'N', 'O', 'P', 'Q', 'R',
         'S', 'T', 'U', 'V', 'W', 'X',
         'Y', 'Z'
     ]
-    morse_code_alpahbets = [
+    morse_code_character = [
+        '--..--', '.-.-.-', '..--..', '-.-.-.',
+        '---...', '-..-.', '-.-.--', '.-.-.',
+        '-....-', '...-..-', '.-...', '.--.-.',
+        '-.--.', '-.--.-', '-----', '.----',
+        '..---', '...--', '....-', '.....',
+        '-....', '--...', '---..', '----.',
         '.-', '-...', '-.-.', '-..', '.', '..-.',
         '--.', '....', '..', '.---', '-.-', '.-..',
         '--', '-.', '---', '.--.', '--.-', '.-.',
         '...', '-', '..-', '...-', '.--', '-..-',
-        '-.-', '--..'
-    ]
+        '-.-', '--..']
 
-    for n in range(len(numbers)):
-        string = string.replace(numbers[n], morse_code_numbers[n])
-
-    for n in range(len(characters)):
-        string = string.replace(characters[n], morse_code_characters[n])
-    
-    for n in range(len(alpahbets)):
-        string = string.replace(alpahbets[n], morse_code_alpahbets[n])
-
+    for n in range(len(character)):
+        string = string.replace(character[n], morse_code_character[n])
     print(string)
 
 
