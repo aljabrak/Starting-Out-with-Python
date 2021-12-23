96ad8cae9aa644409e74758a4c71f903545b0bd4
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-23 10:55:22 +0500

    Small tweaks

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -13,30 +13,39 @@ m = 1
 x_values = []
 
 
-print("--- NUMBER GUESSING GAME ---")
+print("--- NUMBER GUESSING GAME ---\n")
+print("Enter Number Between 1 - 100.")
 
 while m == 1:
     x = randint(MIN, MAX)
     x0 = int(input("Enter Guess Number: x0 = "))
-    x_values.append(x0)
 
-    if x > x0:
-        print("Too Low Try Again...")
+    if x0 >= MIN and x0 <= MAX:
+        x_values.append(x0)
 
-    elif x < x0:
-        print("Too High Try Again...")
+        if x > x0:
+            print("Too Low Try Again...")
 
+        elif x < x0:
+            print("Too High Try Again...")
+
+        else:
+            print("Congartulations!")
+
+            N = len(x_values)
+            if N <= 3:
+                print("Great Guess!")
+            print("Number of Times Guessed: N = ", N)
+            print("Correct Guess: x = ", x0)
+            title("Statistics.")
+            xlabel("Number of Attempts.")
+            ylabel("Guessed Numbers.")
+            hist(x_values, bins = N, ec = "black")
+            show()
+
+            print("\n Press 1 To Start Again...")
+            m = int(input())
+    
     else:
-        print("Congartulations!")
-
-        N = len(x_values)
-        if N <= 3:
-            print("Great Guess!")
-        print("Number of Times Guessed: N = ", N)
-        print("Correct Guess: x = ", x0)
-        title("Statistics.")
-        hist(x_values, bins = N, ec="black")
-        show()
-
-        print("\n Press 1 To Start Again...")
-        m = int(input())
+        print("Error!")
+        print("Enter Number Between 1 - 100.")
